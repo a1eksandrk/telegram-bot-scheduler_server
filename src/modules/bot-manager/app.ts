1550da7ad8164db5e83e230b55a0e1088581201a
AlexKomz <a.komzoltsev@MacBook-Pro.local>
2024-02-11 10:10:39 +0300

    feat: adds swaggerUI support

--- src/modules/bot-manager/app.ts
+++ src/modules/bot-manager/app.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata'
 
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import swagger from '@fastify/swagger'
+import swaggerUi from '@fastify/swagger-ui'
 
 import env from '#shared/configs/env.js'
 
@@ -17,6 +19,13 @@ const init = async (container: AwilixContainer<DI>): Promise<void> => {
   const fastify = Fastify({ logger: true })
 
   fastify.register(cors, { origin: env.ALLOWED_ORIGINS, methods: env.ALLOWED_METHODS })
+  fastify.register(swagger)
+  fastify.register(swaggerUi, {
+    routePrefix: '/documentation',
+    uiConfig: { docExpansion: 'list' },
+    staticCSP: true,
+    transformSpecificationClone: true
+  })
 
   fastify.register(botController.register, { prefix: API_PREFIX })
   fastify.register(chatController.register, { prefix: API_PREFIX })
